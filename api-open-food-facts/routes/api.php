ca4e485f5e6a0762d7c17ae5aeffebd5f0746783
thig71 <thiagodosantos64@gmail.com>
2023-04-24 17:34:15 -0300

    ajuste kernel

--- api-open-food-facts/routes/api.php
+++ api-open-food-facts/routes/api.php
@@ -5,9 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 
-//Route::get('/', [ProductController::class, 'index']);
 Route::put('/products/{code}', [ProductController::class, 'update']);
 Route::delete('/products/{code}', [ProductController::class, 'destroy']);
 Route::get('/products/{code}', [ProductController::class, 'show']);
 Route::get('/products', [ProductController::class, 'index']);
-Route::get('/productsList', [ProductController::class, 'showList']);
+Route::get('/', [ProductController::class, 'showList']);
