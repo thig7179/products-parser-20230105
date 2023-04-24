42b0310d36d442b9b176af05eba7f9091632abe3
thig71 <thiagodosantos64@gmail.com>
2023-04-24 16:09:12 -0300

    insert do db

--- api-open-food-facts/app/WebService/OpenFoodFactsService.php
+++ api-open-food-facts/app/WebService/OpenFoodFactsService.php
@@ -1,7 +1,13 @@
 <?php 
 namespace App\WebService;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\Models\Product;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 
-class OpenFoodFactsService{
+class OpenFoodFactsService extends Eloquent{
+
+    use DatabaseMigrations;
 
     public function getList(){
         $json = file_get_contents('https://static.openfoodfacts.org/data/delta/index.txt');
@@ -11,11 +17,19 @@ class OpenFoodFactsService{
         foreach ($nomes as $value) {
             $url = 'https://static.openfoodfacts.org/data/delta/'.$value[0];
             $file_name = basename($url);
-            if (file_put_contents('../'.$file_name, file_get_contents($url)))
+            if (file_put_contents('./'.$file_name, file_get_contents('compress.zlib://'.$url)))
                 {
                     date_default_timezone_set('America/Sao_Paulo');
                     $data = array("downloaded" => "successfully", "data" => date("Y-m-d H:i:s"), "status" => "1");
-                    return $data;
+                    try{
+                        $insertManyResult = Product::create(['./'.$file_name]);
+                        $data = array("downloaded" => "successfully", "data" => date("Y-m-d H:i:s"), "status" => "1", "count" => $insertManyResult->count());
+                        return $data;
+                    }catch(Exception $e){
+                        echo "Exceção: ".$e->getMessage()."\n";
+                        return $data;
+                    }
+                    
                 }
                 else
                 {
